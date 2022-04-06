338f61aa956cd209e74739b119f8331292744470
CamilaT22 <82045404+CamilaT22@users.noreply.github.com>
2022-04-06 18:44:41 -0500

    Add files via upload

---
+++ Sistema.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+class Sistema{
+	private ArrayList <Usuario> usuarios;
+	
+	public ArrayList <Usuario> getUsuario(){
+		return usuarios;
+	}
+	public void setUsuario(ArrayList <Usuario> usuarios){
+		this.usuarios = usuarios;
+	}
+}
