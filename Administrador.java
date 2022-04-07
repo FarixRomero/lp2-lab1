48f1f6455612d62fcfaaac3faef676aabc4fd981
Angie Centeno Cáceres <82254231+anwili1920@users.noreply.github.com>
2022-04-06 20:27:38 -0500

    PrimerIntento

---
+++ Administrador.java
@@ -0,0 +1,32 @@
+public class Administrador extends Persona {
+    private int codigo;
+    Medico RelacionMedicos[];//relación de medicos que administra
+    
+    void SetCodigo(int codigo){
+        this.codigo=codigo;
+    }
+    int GetCodigo(){
+        return codigo;
+    }
+    void CrearUsuario(){
+     /* Aqui*/
+    }
+    void GenerarListaPacientes(){
+    
+    }
+    void GestionarUsuarios(){
+        /*codigo*/
+    }
+    void GestionarMedicos(){
+        /*codigo*/
+    }
+    void GestionarConsultorios(){
+        /*codigo*/
+    }
+    void GestionarCitas( Cita arreglo[]){
+        /*codigo*/
+    }
+    void GestionarMedicamentos(){
+        /*codigo*/
+    }
+}
