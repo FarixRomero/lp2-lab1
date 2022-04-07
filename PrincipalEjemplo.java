c2b0cf94c4021247470d0f4baeb39987db067f2c
PedroYataco <89876264+PedroYataco@users.noreply.github.com>
2022-04-06 22:19:01 -0500

    Update PrincipalEjemplo.java

--- PrincipalEjemplo.java
+++ PrincipalEjemplo.java
@@ -1,23 +1,25 @@
 import java.util.ArrayList;
 import java.util.Date;
 import java.text.SimpleDateFormat;
+
 class PrincipalEjemplo{
 	public static void main(String[] args) throws Exception{
-		SimpleDateFormat formato = new SimpleDateFormat("dd-MM-yyyy");
+	SimpleDateFormat formato = new SimpleDateFormat("dd-MM-yyyy");
        
        
         Paciente pac1 = new Paciente("CORREO","Farix Romero","JAVOSFOREVER","Activo","Farix","Romero",72634617,69,true,20192222);
         System.out.println(pac1.consultarDatosUsuario());
 		
-
-	Medico med1 = new Medico("CORREO","Juan Perez","JAVOLOVER","Activo","Juan","Perez",1234567,420,0.0F,0,11111);
+	Especialidad esp1 = new Especialidad("ESP001","Médicina Interna");
+		
+	Medico med1 = new Medico("CORREO","Juan Perez","JAVOLOVER","Activo","Juan","Perez",1234567,420,0.0F,0,11111,esp1);
         System.out.println(med1.consultarDatosUsuario());
         
         CitaMedica cita1 = new CitaMedica("INF101","PROGRAMADA",med1,pac1);
         System.out.println(cita1.obtenerDatosCita());
 		
-		HistorialClinico his1 = new HistorialClinico(1234);
-		System.out.println("Numero de historial: " + his1.getNroHistoria());
+	HistorialClinico his1 = new HistorialClinico(1234);
+	System.out.println("Numero de historial: " + his1.getNroHistoria());
 
     }
 }
