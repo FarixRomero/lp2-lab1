567fe0c7bbdf0f8f146070986f47678e318f4d2f
PedroYataco <89876264+PedroYataco@users.noreply.github.com>
2022-04-06 20:00:32 -0500

    Update PrincipalEjemplo.java

--- PrincipalEjemplo.java
+++ PrincipalEjemplo.java
@@ -8,6 +8,13 @@ class PrincipalEjemplo{
        
         Paciente pac1 = new Paciente("CORREO","Farix Romero","JAVOSFOREVER","Activo","Farix","Romero",72634617,69,true,20192222);
         System.out.println(pac1.consultarDatosUsuario());
+		
+
+	Medico med1 = new Medico("CORREO","Juan Perez","JAVOLOVER","Activo","Juan","Perez",1234567,420,0.0F,0,11111);
+        System.out.println(med1.consultarDatosUsuario());
+        
+        CitaMedica cita1 = new CitaMedica("INF101","PROGRAMADA",med1,pac1);
+        System.out.println(cita1.obtenerDatosCita());
 
     }
 }
