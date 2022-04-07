c91d6454ba19c27d5e13be45383894f9cc67420f
IsaiBr <a20191283@pucp.edu.pe>
2022-04-06 20:04:42 -0500

    subida de prueba

---
+++ HistorialClinico.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+class HistorialClinico{
+	//atributos
+	private int nroHistoria;
+	//private ArrayList<Diagnostico> diagnosticos;
+	
+	//constructores
+	public HistorialClinico(){}
+	public HistorialClinico(int nroHistoria){
+		this.nroHistoria = nroHistoria;
+	}
+	
+	//destructores
+	public void finalize(){
+        System.out.println("El objeto se esta destruyendo");
+    }
+	
+	//metodos
+	public int getNroHistoria(){
+		return nroHistoria;
+	}
+	public void setNroHistoria(int nroHistoria){
+		this.nroHistoria = nroHistoria;
+	}
+	/*
+	public ArrayList<Diagnostico> getDiagnostico(){
+		return diagnosticos;
+	}
+	public void setDiagnosticos(){
+		this.diagnosticos = diagnosticos;
+	}
+	*/
+}
