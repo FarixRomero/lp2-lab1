dc07641100483ed39bcb9f4e3ddd4a2ad558494b
Angie Centeno Cáceres <82254231+anwili1920@users.noreply.github.com>
2022-04-27 00:43:51 -0500

    ACTU ANGIE

--- ScriptBD/ScriptPersonal.sql
+++ ScriptBD/ScriptPersonal.sql
@@ -3,6 +3,9 @@ DROP TABLE IF EXISTS Administrador;
 DROP TABLE IF EXISTS Especialidad;
 DROP TABLE IF EXISTS Usuario;
 DROP TABLE IF EXISTS Persona;
+DROP TABLE IF EXISTS Semestre;
+DROP TABLE IF EXISTS Horario;
+DROP TABLE IF EXISTS Consultorio;
 
 CREATE TABLE Especialidad(
 	id_especialidad INT NOT NULL AUTO_INCREMENT,
@@ -53,5 +56,28 @@ CREATE TABLE Medico(
     FOREIGN KEY(fid_especialidad) REFERENCES Especialidad(id_especialidad),
     FOREIGN KEY(fid_usuario) REFERENCES Usuario(id_usuario)
 )ENGINE = InnoDB;
+CREATE TABLE Semestre(
+	id_semestre INT AUTO_INCREMENT,
+    nombre  VARCHAR(50),
+    fechaInicio DATE,
+    fechaFin DATE,
+    PRIMARY KEY(id_semestre)
+)ENGINE=InnoDB;
 
+CREATE TABLE Horario(
+	id_horario INT AUTO_INCREMENT,
+    dia  VARCHAR(50),
+    hora_inicio DATE,
+    hora_fin DATE,
+    fid_semestre INT,
+    fid_medico INT,
+    PRIMARY KEY(id_horario),
+    FOREIGN KEY(fid_medico) REFERENCES Medico(id_medico),
+    FOREIGN KEY(fid_semestre) REFERENCES Semestre(id_semestre)
+)ENGINE=InnoDB;
+CREATE TABLE Consultorio(
+	id_consultorio INT AUTO_INCREMENT,
+    disponible BOOL,
+    PRIMARY KEY(id_consultorio)
+)ENGINE=InnoDB;
 
