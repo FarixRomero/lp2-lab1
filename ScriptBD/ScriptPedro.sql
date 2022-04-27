4712003ab1f30079068ca920702d21571dc9fecd
PedroYataco <89876264+PedroYataco@users.noreply.github.com>
2022-04-27 00:58:34 -0500

    Update ScriptPedro.sql

--- ScriptBD/ScriptPedro.sql
+++ ScriptBD/ScriptPedro.sql
@@ -5,14 +5,20 @@ DROP TABLE IF EXISTS HistorialClinico;
 
 CREATE TABLE Comentario (
     descripcion VARCHAR(50),
-    fecha_comentario DATE
+    fecha_comentario DATE,
+    fid_medico INT,
+    FOREIGN KEY(fid_medico) REFERENCES Medico(id_medico)
 ) ENGINE = InnoDB;
 
 CREATE TABLE Chat (
     comentario VARCHAR(200),
     fecha DATE,
     emisor VARCHAR(50),
-    hora TIME
+    hora TIME,
+    fid_medico INT,
+    fid_paciente INT,
+    FOREIGN KEY(fid_medico) REFERENCES Medico(id_medico),
+    FOREIGN KEY(fid_paciente) REFERENCES Paciente(id_paciente)
 ) ENGINE = InnoDB;
 
 CREATE TABLE HistorialClinico (
