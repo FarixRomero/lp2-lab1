e62dd478a65105edbe644a65a2f4ea22482356da
FarixRomeroCode <a20193387@pucp.edu.pe>
2022-04-27 19:50:49 -0500

    exm

---
+++ ProyectoJava/LP2ClinicaGestionMedicaController/src/pe/edu/pucp/lp2Clinica/GestionMedica/dao/Ejemplo.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package pe.edu.pucp.lp2Clinica.GestionMedica.dao;
+
+/**
+ *
+ * @author farix.romero
+ * Autor: Farix Romero Manrique 20193387
+ */
+public class Ejemplo {
+
+}
