1abd633128c1ef0f7afe96a9bd0f16317dd3cee8
CamilaT22 <82045404+CamilaT22@users.noreply.github.com>
2022-04-27 23:23:06 -0500

    Update ScriptPedro.sql

--- ScriptBD/ScriptPedro.sql
+++ ScriptBD/ScriptPedro.sql
@@ -4,19 +4,23 @@ DROP TABLE IF EXISTS HistorialClinico;
 
 
 CREATE TABLE Comentario (
+    id_comentario INT,
     descripcion VARCHAR(50),
     fecha_comentario DATE,
     fid_medico INT,
+    PRIMARY KEY(id_comentario),
     FOREIGN KEY(fid_medico) REFERENCES Medico(id_medico)
 ) ENGINE = InnoDB;
 
 CREATE TABLE Chat (
+    id_chat INT,
     comentario VARCHAR(200),
     fecha DATE,
     emisor VARCHAR(50),
     hora TIME,
     fid_medico INT,
     fid_paciente INT,
+    PRIMARY KEY(id_chat),
     FOREIGN KEY(fid_medico) REFERENCES Medico(id_medico),
     FOREIGN KEY(fid_paciente) REFERENCES Paciente(id_paciente)
 ) ENGINE = InnoDB;
