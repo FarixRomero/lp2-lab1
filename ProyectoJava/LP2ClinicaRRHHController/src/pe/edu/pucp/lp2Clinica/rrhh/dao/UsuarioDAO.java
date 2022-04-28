3454ad7e1901e3c2502a7628c3b49157716b9eda
Equipo <Equipo@AngieCienta>
2022-04-27 20:53:10 -0500

    Santi regresa :( porfas

--- ProyectoJava/LP2ClinicaRRHHController/src/pe/edu/pucp/lp2Clinica/rrhh/dao/UsuarioDAO.java
+++ ProyectoJava/LP2ClinicaRRHHController/src/pe/edu/pucp/lp2Clinica/rrhh/dao/UsuarioDAO.java
@@ -2,11 +2,15 @@
 
 package pe.edu.pucp.lp2Clinica.rrhh.dao;
 
+import pe.edu.pucp.clinica.personal.model.Usuario;
+
 /**
  *
  * @author Angie Centeno Cáceres
     20180214
  */
 public interface  UsuarioDAO {
-
+    int insertar( Usuario User);
+    int modificar( Usuario User );
+    int eliminar( Usuario User);
 }
